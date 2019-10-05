05fc8099e0e2a1d12c0e2aa7c209f03e0b2aae9c
Menno <mvdjagt1@gmail.com>
2019-10-05 10:14:07 +0200

    added assignment 5 of week 4
    I had some trouble with the make file, if I did make clean it did not cleaned the generator.o and the generator.exe so I wrote a custom cleaner that is called by writing "make cleaner" it just "rm" those files including the generated file.

---
+++ Week 4/Opdracht 5/main.cpp
@@ -0,0 +1,15 @@
+#include "hwlib.hpp"
+
+extern "C" void print( const char c ){
+   hwlib::cout << c;
+}
+
+extern "C" char decompress();
+
+int main( void ){
+
+   hwlib::wait_ms( 500 );
+
+   decompress();
+
+}
